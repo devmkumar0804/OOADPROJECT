714c185c43351c60b32d8e6ed3c8057e465ee7e1
devmkumar0804 <73732254+devmkumar0804@users.noreply.github.com>
2023-05-07 11:38:04 +0530

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,64 @@
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("bellandur","rajajinagar",10.0,"cab",22.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("bellandur","rrnagar",10.0,"cab",20.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("bellandur","kanakpura",10.0,"cab",41.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("bellandur","rajajinagar",5.0,"auto",22.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("bellandur","rrnagar",5.0,"auto",20.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("bellandur","kanakpura",5.0,"auto",41.0);
+
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rajajinagar","bellandur",10.0,"cab",22.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rajajinagar","rrnagar",10.0,"cab",13.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rajajinagar","kanakpura",10.0,"cab",37.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rajajinagar","bellandur",5.0,"auto",22.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rajajinagar","rrnagar",5.0,"auto",13.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rajajinagar","kanakpura",5.0,"auto",37.0);
+
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rrnagar","bellandur",10.0,"cab",22.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rrnagar","rajajinagar",10.0,"cab",13.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rrnagar","kanakpura",10.0,"cab",24.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rrnagar","bellandur",5.0,"auto",22.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rrnagar","rajajinagar",5.0,"auto",13.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("rrnagar","kanakpura",5.0,"auto",24.0);
+
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("kanakpura","bellandur",10.0,"cab",41.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("kanakpura","rajajinagar",10.0,"cab",37.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("kanakpura","rrnagar",10.0,"cab",24.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("kanakpura","bellandur",5.0,"auto",41.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("kanakpura","rajajinagar",5.0,"auto",37.0);
+insert into dist (sourc,destination,rateperkm,ride_type,distance)
+values("kanakpura","rrnagar",5.0,"auto",24.0);
+
+-- select * from dist
+insert into utils values("water",700.0,50.0,"1");
+insert into utils values("water",600.0,10.0,"2");
+insert into utils values("water",550.0,10.0,"3");
+insert into utils values("water",900.0,20.0,"4");
+insert into utils values("water",200.0,20.0,"5");
+
+insert into utils values("electricity",1000.0,10.0,"1");
+insert into utils values("electricity",1200.0,10.0,"2");
+insert into utils values("electricity",1500.0,0.0,"3");
+insert into utils values("electricity",1200.0,2.0,"4");
+insert into utils values("electricity",900.0,100.0,"5");
